6e0d0930c4ff158d9f28fa8312ca387733d0543b
Idriss Neumann <idriss.neumann@comwork.io>
2023-01-13 12:30:41 +0100

    Missing bucket region

--- utils/minio.py
+++ utils/minio.py
@@ -4,6 +4,7 @@ from minio import Minio
 from utils.common import is_disabled
 
 BUCKET_URL = os.getenv('BUCKET_URL')
+BUCKET_REGION = os.getenv('BUCKET_REGION')
 BUCKET_ACCESS_KEY = os.getenv('BUCKET_ACCESS_KEY')
 BUCKET_SECRET_KEY = os.getenv('BUCKET_SECRET_KEY')
 BUCKET_NAME = os.getenv('BUCKET_NAME')
@@ -11,11 +12,11 @@ BUCKET_FOLDER = os.getenv('BUCKET_FOLDER')
 
 minioClient = None
 
-if not any(is_disabled(setting) for setting in [BUCKET_URL, BUCKET_ACCESS_KEY, BUCKET_SECRET_KEY, BUCKET_NAME, BUCKET_FOLDER]):
+if not any(is_disabled(setting) for setting in [BUCKET_URL, BUCKET_REGION, BUCKET_ACCESS_KEY, BUCKET_SECRET_KEY, BUCKET_NAME, BUCKET_FOLDER]):
     minioClient = Minio(BUCKET_URL,
+                    region=BUCKET_REGION,
                     access_key=BUCKET_ACCESS_KEY,
-                    secret_key=BUCKET_SECRET_KEY,
-                    secure=True)
+                    secret_key=BUCKET_SECRET_KEY)
 
 def get_minio_client():
     return minioClient
