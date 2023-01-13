94dfef39c4581124658bf9c0b6699e9c04e0bd00
Idriss Neumann <idriss.neumann@comwork.io>
2023-01-13 11:59:18 +0100

    List files from the bucket

--- utils/bridge.py
+++ utils/bridge.py
@@ -2,9 +2,14 @@ import os
 
 from time import sleep
 from utils.logger import log_msg
+from utils.minio import get_bucket_folder, get_bucket_name, get_minio_client
 
 WAIT_TIME = int(os.environ['WAIT_TIME'])
 
 def bridge():
     log_msg("INFO", "[storage-bridge] deployment of version {} !".format(os.environ['STORAGE_BRIDGE_VERSION']))
+    minioClient = get_minio_client()
+    files = minioClient.list_objects(get_bucket_name(), prefix=get_bucket_folder(), recursive=True)
+    for file in files:
+        log_msg("INFO", "[storage-bridge] processing file {}".format(file))
     sleep(WAIT_TIME)
--- utils/minio.py
+++ utils/minio.py
@@ -6,10 +6,12 @@ from utils.common import is_disabled
 BUCKET_URL = os.getenv('BUCKET_URL')
 BUCKET_ACCESS_KEY = os.getenv('BUCKET_ACCESS_KEY')
 BUCKET_SECRET_KEY = os.getenv('BUCKET_SECRET_KEY')
+BUCKET_NAME = os.getenv('BUCKET_NAME')
+BUCKET_FOLDER = os.getenv('BUCKET_FOLDER')
 
 minioClient = None
 
-if not any(is_disabled(setting) for setting in [BUCKET_URL, BUCKET_ACCESS_KEY, BUCKET_SECRET_KEY]):
+if not any(is_disabled(setting) for setting in [BUCKET_URL, BUCKET_ACCESS_KEY, BUCKET_SECRET_KEY, BUCKET_NAME, BUCKET_FOLDER]):
     minioClient = Minio(os.environ['BUCKET_URL'],
                     access_key=os.environ['BUCKET_ACCESS_KEY'],
                     secret_key=os.environ['BUCKET_SECRET_KEY'],
@@ -17,3 +19,12 @@ if not any(is_disabled(setting) for setting in [BUCKET_URL, BUCKET_ACCESS_KEY, B
 
 def get_minio_client():
     return minioClient
+
+def get_bucket_name():
+    return BUCKET_NAME
+
+def get_bucket_folder():
+    if is_disabled(BUCKET_FOLDER):
+        return None
+
+    return "{}/".format(BUCKET_FOLDER)
