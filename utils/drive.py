0e9d168f44f08ce418fe2f01023d3dcc62fb4df1
Idriss Neumann <idriss.neumann@comwork.io>
2023-01-15 14:59:55 +0100

    remove useless import

--- utils/drive.py
+++ utils/drive.py
@@ -6,7 +6,7 @@ from google.oauth2.service_account import Credentials
 from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
 from googleapiclient.http import MediaFileUpload
-from utils.common import is_disabled, is_empty, is_not_empty
+from utils.common import is_disabled
 from utils.logger import log_msg
 
 SA_B64 = os.getenv('GOOGLE_SA_B64')
